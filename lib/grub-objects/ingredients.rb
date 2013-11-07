4f04fa845b399b1a3f322f141af57f81f0cab332
tony <amirsgarbunk@gmail.com>
2013-11-06 21:06:39 -0800

    test ingredient name lowercased, test no-dupe ingredient names

--- lib/grub-objects/ingredients.rb
+++ lib/grub-objects/ingredients.rb
@@ -1,2 +1,9 @@
-class Ingredients
+require 'mongooz'
+
+class Ingredients<Mongooz::MongoozHash
+	alias_method :old_db_insert, :db_insert
+	def db_insert(options={})
+		self[:name]=self[:name].downcase if self[:name]
+		old_db_insert(options)
+	end
 end
--- specs/ingredients-spec.rb
+++ specs/ingredients-spec.rb
@@ -1,8 +1,28 @@
 require 'grub-objects'
 
+Mongooz.defaults(:db=>'grub_test')
+
 describe Ingredients do
-	it "runs" do
-		a=Ingredients.new
-		puts "done"
+	describe "#db_insert" do
+		before :all do
+			@test_ingredient=Ingredients.new
+			@test_ingredient_name="FOO"
+			@test_ingredient[:name]=@test_ingredient_name
+			@test_ingredient.db_insert
+		end
+		after :all do
+			@test_ingredient.db_delete
+		end
+
+		it "lowercases the ingredient name" do
+			expect(@test_ingredient[:name]).to_not eq(@test_ingredient_name)
+			expect(@test_ingredient[:name]).to eq(@test_ingredient_name.downcase)
+		end
+
+		it "doesn't allow dupe ingredient names" do
+			dupe=Ingredients.new
+			dupe[:name]=@test_ingredient_name
+			expect{dupe.db_insert}.to raise_error
+		end
 	end
 end
