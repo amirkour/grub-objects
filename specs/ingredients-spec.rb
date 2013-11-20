387ab9abe37797a5489618a78a4b87ef246613f4
tony <amirsgarbunk@gmail.com>
2013-11-19 20:51:29 -0800

    adding specs for ingredient equality

--- specs/ingredients-spec.rb
+++ specs/ingredients-spec.rb
@@ -25,4 +25,72 @@ describe Ingredients do
 			expect{dupe.db_insert}.to raise_error
 		end
 	end
+	describe "#==" do
+		it "returns true for the same object" do
+			a=Ingredients.new :name=>"foo", :composites=>[{:name=>"bar"}]
+			b=a
+			expect(a).to eq(b)
+		end
+		it "returns true for two hashes that are ==" do
+			a=Ingredients.new :name=>"foo", :composites=>[{:name=>"bar"}]
+			b=Ingredients.new :name=>"foo", :composites=>[{:name=>"bar"}]
+			expect(a).to eq(b)
+		end
+		it "returns true symmetrically" do
+			a=Ingredients.new :name=>"foo", :composites=>[{:name=>"bar"}]
+			b=Ingredients.new :name=>"foo", :composites=>[{:name=>"bar"}]
+			expect(a).to eq(b)
+			expect(b).to eq(a)
+		end
+		it "returns false when comparing an ingredient to a non-ingredient" do
+			a=Ingredients.new :name=>"foo", :composites=>[{:name=>"bar"}]
+			b=Object.new
+			expect(a).to_not eq(b)
+		end
+		it "returns false if two ingredients are not equal" do
+			a=Ingredients.new :name=>"foo", :composites=>[{:name=>"bar"}]
+			b=Ingredients.new :name=>"foo", :composites=>[{:name=>"baz"}]
+			expect(a).to_not eq(b)
+		end
+		it "returns false symmetrically" do
+			a=Ingredients.new :name=>"foo", :composites=>[{:name=>"bar"}]
+			b=Ingredients.new :name=>"foo", :composites=>[{:name=>"baz"}]
+			expect(a).to_not eq(b)
+			expect(b).to_not eq(a)
+		end
+	end
+	describe "#eql?" do
+		it "returns true for the same object" do
+			a=Ingredients.new :name=>"foo", :composites=>[{:name=>"bar"}]
+			b=a
+			expect(a).to eql(b)
+		end
+		it "returns true for two hashes that are ==" do
+			a=Ingredients.new :name=>"foo", :composites=>[{:name=>"bar"}]
+			b=Ingredients.new :name=>"foo", :composites=>[{:name=>"bar"}]
+			expect(a).to eql(b)
+		end
+		it "returns true symmetrically" do
+			a=Ingredients.new :name=>"foo", :composites=>[{:name=>"bar"}]
+			b=Ingredients.new :name=>"foo", :composites=>[{:name=>"bar"}]
+			expect(a).to eql(b)
+			expect(b).to eql(a)
+		end
+		it "returns false when comparing an ingredient to a non-ingredient" do
+			a=Ingredients.new :name=>"foo", :composites=>[{:name=>"bar"}]
+			b=Object.new
+			expect(a).to_not eql(b)
+		end
+		it "returns false if two ingredients are not equal" do
+			a=Ingredients.new :name=>"foo", :composites=>[{:name=>"bar"}]
+			b=Ingredients.new :name=>"foo", :composites=>[{:name=>"baz"}]
+			expect(a).to_not eql(b)
+		end
+		it "returns false symmetrically" do
+			a=Ingredients.new :name=>"foo", :composites=>[{:name=>"bar"}]
+			b=Ingredients.new :name=>"foo", :composites=>[{:name=>"baz"}]
+			expect(a).to_not eql(b)
+			expect(b).to_not eql(a)
+		end
+	end
 end
